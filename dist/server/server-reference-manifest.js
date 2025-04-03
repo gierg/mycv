76c05f11d4b80983097e312d7266457af2cd3e93
Gie Rg <gie.gama90@gmail.com>
2025-04-03 22:45:23 +0700

    Deploy

--- dist/server/server-reference-manifest.js
+++ dist/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"Qj/izFuIJ2tAPRrPERVMvRN0W5jFd+cidxhpIryca2Y=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
